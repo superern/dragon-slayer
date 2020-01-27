f8bd19064fbc5c2b44bfdfeead73c5e82601e081
Superern <ernandrewgregorio@gmail.com>
2020-01-27 15:26:05 +0800

    Add relationship and helper method in Comment model

--- app/Comment.php
+++ app/Comment.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     protected $fillable = ['description', 'game_id'];
+
+    # Relationships
+
+    public function game() {
+        return $this->belongsTo(Game::class);
+    }
+    
+    public function attachGame(Game $game) {
+        return $this->game()->associate($game);
+    }
 }
