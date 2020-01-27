d52a034e8adfa90f3d807a7862e8de988561d77d
Superern <ernandrewgregorio@gmail.com>
2020-01-27 14:39:56 +0800

    add game feature
    skeleton

---
+++ app/Http/Controllers/GameController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Game;
+use Illuminate\Http\Request;
+
+class GameController extends Controller
+{
+    public function index()
+    {
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Game $game)
+    {
+    }
+
+    public function destroy(Game $game)
+    {
+    }
+}
