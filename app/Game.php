93848fc4efe089482ed6e69fda501577d9fe2833
Superern <ernandrewgregorio@gmail.com>
2020-01-27 15:23:43 +0800

    Add relationship and helper method in Game model

--- app/Game.php
+++ app/Game.php
@@ -7,4 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     protected $fillable = ['name', 'status', 'user_id'];
+
+    # Relationships
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function attachUser(User $user) {
+        return $this->user()->associate($user);
+    }
 }
