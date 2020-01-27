4ff6d497ac22c68bd92833c394ffd339b54dffc9
Superern <ernandrewgregorio@gmail.com>
2020-01-27 14:48:10 +0800

    add comment feature
    skeleton

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+
+    public function store(Request $request)
+    {
+    }
+}
---
+++ database/migrations/2020_01_27_064044_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->longText('description');
+
+            $table->unsignedBigInteger('game_id');
+            $table->foreign('game_id')
+                ->references('id')
+                ->on('games')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
