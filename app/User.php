608184407327cd376b56799db4f40f396ff213df
Superern <ernandrewgregorio@gmail.com>
2020-01-27 15:05:08 +0800

    Add relationship and helper method in User model

--- app/User.php
+++ app/User.php
@@ -36,4 +36,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    # Relationships
+
+    public function games() {
+        return $this->hasMany(Game::class);
+    }
+
+    public function addGame(Game $game) {
+        return $this->games()->save($game);
+    }
 }
